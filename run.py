2732dc5038cfdd4acb2775373958caf2c891fc90
huangqinjin <huangqinjin@gmail.com>
2019-11-19 21:12:33 +0800

    run.py supports set/get variables

--- run.py
+++ run.py
@@ -6,13 +6,20 @@ import sys
 try: input = raw_input
 except NameError: pass
 
-a = 1
+vars = {
+    "PCLI_PR": "",
+    "PCLI_PP": "",
+    "PCLI_ID": "",
+}
 while True:
-    line = input('>')
-    if line == "exit":
+    args = input('>').split()
+    if args[0] == "exit":
         break
+    elif args[0] == "Set":
+        vars[args[1][2:]] = args[2].replace('"', '')
+    elif args[0] == "Echo":
+        print(args[1].format(**vars).replace('$', ''))
     else:
-        print("Echo {}".format(line))
-        print("Error Code: {}".format(a))
-        sys.stdout.flush()
-        a = a + 1
+        print("Echo {}".format(" ".join(args)))
+    print("Error Code: {}".format(0))
+    sys.stdout.flush()
