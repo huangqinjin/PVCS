9f16f76f0f31a050a20ab4200d78985a0bceb70a
huangqinjin <huangqinjin@gmail.com>
2019-12-02 23:51:48 +0800

    refine python script

--- run.py
+++ run.py
@@ -12,14 +12,21 @@ vars = {
     "PCLI_ID": "",
 }
 while True:
-    args = input('>').split()
+    sys.stdout.write('>')
+    sys.stdout.flush()
+    args = input().split()
+    args[0] = args[0].lower()
     if args[0] == "exit":
         break
-    elif args[0] == "Set":
+    elif args[0] == "set":
         vars[args[1][2:]] = args[2].replace('"', '')
-    elif args[0] == "Echo":
+    elif args[0] == "echo":
         print(args[1].format(**vars).replace('$', ''))
+    elif args[0] == "ask":
+        sys.stdout.write("Right? (y/n) ")
+        sys.stdout.flush()
+        print("Answer: " + input())
     else:
-        print("Echo {}".format(" ".join(args)))
-    print("Error Code: {}".format(0))
+        print(" ".join(args).upper())
+    sys.stdout.write("Error Code: 0")
     sys.stdout.flush()
