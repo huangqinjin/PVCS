ca8f3dc146a4c71be938c0441b77846a88ce0817
huangqinjin <huangqinjin@gmail.com>
2019-12-05 23:34:13 +0800

    python catch all exceptions

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import sys
+import traceback
 
 #Works in Python 2 and 3:
 try: input = raw_input
@@ -14,19 +15,22 @@ vars = {
 while True:
     sys.stdout.write('>')
     sys.stdout.flush()
-    args = input().split()
-    args[0] = args[0].lower()
-    if args[0] == "exit":
-        break
-    elif args[0] == "set":
-        vars[args[1][2:]] = args[2].replace('"', '')
-    elif args[0] == "echo":
-        print(args[1].format(**vars).replace('$', ''))
-    elif args[0] == "ask":
-        sys.stdout.write("Right? (y/n) ")
-        sys.stdout.flush()
-        print("Answer: " + input())
-    else:
-        print(" ".join(args).upper())
-    sys.stdout.write("Error Code: 0")
-    sys.stdout.flush()
+    try:
+        args = input().split()
+        args[0] = args[0].lower()
+        if args[0] == "exit":
+            break
+        elif args[0] == "set":
+            vars[args[1][2:]] = args[2].replace('"', '')
+        elif args[0] == "echo":
+            print(args[1].format(**vars).replace('$', ''))
+        elif args[0] == "ask":
+            sys.stdout.write("Right? (y/n) ")
+            sys.stdout.flush()
+            print("Answer: " + input())
+        else:
+            print(" ".join(args).upper())
+        sys.stdout.write("Error Code: 0")
+    except:
+        traceback.print_exc()
+        sys.stdout.write("Error Code: -1")
